5a1c030af4a3f9e036a4cb73da59efad99483337
nicoleee0114 <423001158@ntc.edu.ph>
2026-05-05 00:49:47 +0800

    Add pagination

--- app/Controllers/Students.php
+++ app/Controllers/Students.php
@@ -1,26 +1,27 @@
 <?php
+
 namespace App\Controllers;
+
 use App\Models\StudentModel;
 
-class Students extends BaseController {
+class Students extends BaseController
+{
 
-    public function index() {
+    public function index()
+    {
         $model = new StudentModel();
-        $search = $this->request->getGet('search');
-        if ($search) {
-            $model->like('name', $search)->orLike('course', $search);
-        }
         $data['students'] = $model->paginate(5);
         $data['pager'] = $model->pager;
-        $data['search'] = $search;
         return view('students/index', $data);
     }
 
-    public function create() {
+    public function create()
+    {
         return view('students/create');
     }
 
-    public function store() {
+    public function store()
+    {
         $model = new StudentModel();
         $model->save([
             'name'   => $this->request->getPost('name'),
@@ -30,13 +31,15 @@ class Students extends BaseController {
         return redirect()->to('/students');
     }
 
-    public function edit($id) {
+    public function edit($id)
+    {
         $model = new StudentModel();
         $data['student'] = $model->find($id);
         return view('students/edit', $data);
     }
 
-    public function update($id) {
+    public function update($id)
+    {
         $model = new StudentModel();
         $model->update($id, [
             'name'   => $this->request->getPost('name'),
@@ -46,7 +49,8 @@ class Students extends BaseController {
         return redirect()->to('/students');
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $model = new StudentModel();
         $model->delete($id);
         return redirect()->to('/students');
