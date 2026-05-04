5d2a50068c102b960778ad6e915371f4e347f350
423002013 <423002013@ntc.edu.ph>
2026-05-05 00:43:18 +0800

    Add search feature

--- app/Controllers/Students.php
+++ app/Controllers/Students.php
@@ -5,16 +5,17 @@ use App\Models\StudentModel;
 class Students extends BaseController {
 
     public function index() {
-        $model = new StudentModel();
-        $search = $this->request->getGet('search');
-        if ($search) {
-            $model->like('name', $search)->orLike('course', $search);
-        }
-        $data['students'] = $model->paginate(5);
-        $data['pager'] = $model->pager;
-        $data['search'] = $search;
-        return view('students/index', $data);
+    $model = new StudentModel();
+    $search = $this->request->getVar('search');
+    if ($search) {
+        $data['students'] = $model->where('name', $search)->orWhere('course', $search)->findAll();
+    } else {
+        $data['students'] = $model->findAll();
     }
+    $data['search'] = $search;
+    return view('students/index', $data);
+}
+
 
     public function create() {
         return view('students/create');
@@ -38,7 +39,8 @@ class Students extends BaseController {
 
     public function update($id) {
         $model = new StudentModel();
-        $model->update($id, [
+        $model->save([
+            'id'     => $id,
             'name'   => $this->request->getPost('name'),
             'email'  => $this->request->getPost('email'),
             'course' => $this->request->getPost('course'),
@@ -48,7 +50,7 @@ class Students extends BaseController {
 
     public function delete($id) {
         $model = new StudentModel();
-        $model->delete($id);
+        $model->where('id', $id)->delete();
         return redirect()->to('/students');
     }
 }
